96485abe29d501d6c1b89eac4833dde5d58c3fbd
Mytram <mytram2@gmail.com>
2014-11-21 22:07:52 +1100

    Issue#2 - Missing a valid license
    - Added artistic_2 license in response to a request from Angel Axel

--- lib/Getopt/ArgParse/ActionAppend.pm
+++ lib/Getopt/ArgParse/ActionAppend.pm
@@ -33,3 +33,18 @@ sub apply {
 
 1;
 
+=head1 AUTHOR
+
+Mytram <mytram2@gmail.com> (original author)
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is Copyright (c) 2014 by Mytram.
+
+This is free software, licensed under:
+
+  The Artistic License 2.0 (GPL Compatible)
+
+=cut
+
+
